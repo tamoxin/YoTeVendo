b62eb64b89fff99294957540d1b187644ad4f202
Marco Carrizales <marco_carrizales@msn.com>
2016-04-19 01:51:50 -0500

    Getting started
    Added a very basic model for the interface.

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Yo te vendo</title>
+
+	<link rel="stylesheet" href="assets/demo.css">
+	<link rel="stylesheet" href="assets/header-login-signup.css">
+	<link rel="stylesheet" href="assets/footer-distributed-with-address-and-phones.css">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+	
+
+</head>
+
+<body>
+
+<header class="header-login-signup">
+
+	<div class="header-limiter">
+
+		<h1><a href="index.php">YoTe<span>Vendo</span></a></h1>
+
+		<ul>
+			<li><a href="#">Inicio</a></li>
+			<li><a href="#">Anunciantes</a></li>
+			<li><a href="login.php">Iniciar sesión / Registrarse</a></li>
+		</ul>
+
+	</div>
+
+</header>
+
+<!-- The content of your page would go here. -->
+
+<footer class="footer-distributed">
+
+	<div class="footer-left">
+
+		<h3>YoTe<span>Vendo</span></h3>
+
+		<p class="footer-links">
+			<a href="#">Inicio</a>
+			·
+			<a href="#">Anunciantes</a>
+			·
+			<a href="#">Acerca de</a>
+			·
+			<a href="#">Preguntas frecuentes</a>
+			·
+			<a href="#">Contacto</a>
+		</p>
+
+		<p class="footer-company-name">Yo te vendo &copy; 2016</p>
+	</div>
+
+	<div class="footer-center">
+
+		<div>
+			<i class="fa fa-map-marker"></i>
+			<p><span>Tampico</span> Tamapulipas, México</p>
+		</div>
+
+		<div>
+			<i class="fa fa-phone"></i>
+			<p>+1 555 123456</p>
+		</div>
+
+		<div>
+			<i class="fa fa-envelope"></i>
+			<p><a href="mailto:support@company.com">info@yotevendo.com</a></p>
+		</div>
+
+	</div>
+
+	<div class="footer-right">
+
+		<p class="footer-company-about">
+			<span>Acerca de la compañía</span>
+			Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+		</p>
+
+		<div class="footer-icons">
+
+			<a href="#"><i class="fa fa-facebook"></i></a>
+			<a href="#"><i class="fa fa-twitter"></i></a>
+			<a href="#"><i class="fa fa-linkedin"></i></a>
+			<a href="#"><i class="fa fa-github"></i></a>
+
+		</div>
+
+	</div>
+
+</footer>
+
+</body>
+
+</html>
